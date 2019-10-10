4b0e2967ca9e656220ce3b9daf0949a37636ef85
Your Name <you@example.com>
2019-10-10 16:06:49 +0530

    second commit

---
+++ prog20.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{int a;
+{
+printf("enter any number");
+scanf("%d",&a);
+if(a%2==1)
+printf("%d is odd number",a);
+else
+printf("%dis even number",a);
+}
+return 0;}
