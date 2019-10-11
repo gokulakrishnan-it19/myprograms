b260ab1bc53d164eb7a5f2175b8f5772e3f1c57c
Your Name <you@example.com>
2019-10-11 12:03:25 +0530

    first commit

---
+++ pn.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter any number for a ");
+scanf("%d",&a);
+if (a<0)
+{
+printf ("%d is a negative number",a);}
+else
+{
+printf ("%d is a positive number",a);}
+}
